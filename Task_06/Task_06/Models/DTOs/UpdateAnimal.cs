bdb01f874b6127dfbc8dbd73348aaf7b38b571f2
kilim <s28346@pjwstk.edu.pl>
2024-04-18 23:30:56 +0200

    k

---
+++ Task_06/Task_06/Models/DTOs/UpdateAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_06.Models.DTOs;
+
+public class UpdateAnimal
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Name { get; set; }
+
+    [MaxLength(200)]
+    public string Description { get; set; }
+
+    [MaxLength(100)]
+    public string Category { get; set; }
+
+    [MaxLength(100)]
+    public string Area { get; set; }
+}
